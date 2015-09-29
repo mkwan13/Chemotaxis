d4427e928e3931641b47b95ca0331f174f2473eb
Emily <emilykwan1@gmail.com>
2015-09-29 03:59:54 -0700

    finished

--- build-tmp/source/Chemotaxis.java
+++ build-tmp/source/Chemotaxis.java
@@ -1,8 +1,24 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Chemotaxis extends PApplet {
+
  //declare bacteria variables here   
  Bacteria [] colony;
  Bacteria bact;
 
- void setup()   
+ public void setup()   
 {
     // initialize bacteria variables here
  	size(500, 500);
@@ -14,7 +30,7 @@
  	}  
  }   
 
- void draw()   
+ public void draw()   
  {    
  	background(0);
  	for (int i = 0; i < colony.length; i++)
@@ -37,11 +53,11 @@
  		bY = (int)(Math.random()*500); 
  	}
 
- 	void move()
+ 	public void move()
  	{
  		// pink circles
- 		pX = pX + (int)(Math.random()*2-1.5);
- 		pY = pY + (int)(Math.random()*2-1.5);
+ 		pX = pX + (int)(Math.random()*2-1.5f);
+ 		pY = pY + (int)(Math.random()*2-1.5f);
  		if(pX > bX)
  		{
  			pX = pX + (int)(Math.random()*1-1);
@@ -52,16 +68,16 @@
  		}
  		if(pX < bX)
  		{
- 			pX = pX + (int)(Math.random()*1+.5);
+ 			pX = pX + (int)(Math.random()*1+.5f);
  		}
  		if(pY < bX)
  		{
- 			pY = pY + (int)(Math.random()*1+.5);
+ 			pY = pY + (int)(Math.random()*1+.5f);
  		}
 
  		// blue circles
- 		bX = bX + (int)(Math.random()*2-1.5);
- 		bY = bY + (int)(Math.random()*2-1.5);
+ 		bX = bX + (int)(Math.random()*2-1.5f);
+ 		bY = bY + (int)(Math.random()*2-1.5f);
  		if(bX > pX)
  		{
  			bX = bX + (int)(Math.random()*1-1);
@@ -72,11 +88,11 @@
  		}
  		if(bX < pX)
  		{
- 			bX = bX + (int)(Math.random()*1+.5);
+ 			bX = bX + (int)(Math.random()*1+.5f);
  		}
  		if(bY < pY)
  		{
- 			bY = bY + (int)(Math.random()*1+.5);
+ 			bY = bY + (int)(Math.random()*1+.5f);
  		}
 
  		if(pX == bX && pY == bY)
@@ -88,7 +104,7 @@
  		}
  	}
 
- 	void show()
+ 	public void show()
  	{
  		stroke(255, 0, 255);
  		ellipse(pX, pY, 6, 6);
@@ -98,3 +114,12 @@
  	}
 
  }    
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Chemotaxis" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
